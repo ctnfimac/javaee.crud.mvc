a908cbf7b29b23da239a7535611d90745d3d49a1
Christian Peralta <christianipc.1987@gmail.com>
2019-02-04 01:59:20 -0300

    agrego base de datos y tabla de personajes

---
+++ db/dbz.sql
@@ -0,0 +1,19 @@
+create database if not exists dbz;
+
+use dbz;
+
+create table personaje(
+	id int AUTO_INCREMENT,
+	imagen varchar(50) not null,
+	nombre varchar(30) not null,
+	descripcion varchar(100)  default 'sin descripcion',
+	ataque varchar(30) default 'desconocido',
+	primary key (id)
+);
+
+INSERT INTO personaje (imagen, nombre, descripcion, ataque) values 
+('./img/goku.jpg','Goku','guerrero saiyan personaje principal','kame-hame-ha'),
+('./img/vegeta.jpg','Vegeta','guerrero saiyan principe del planeta vegeta','big-bang'),
+('./img/freezer.jpg','Freezer','Malvado emperador del mal','hakay'),
+('./img/picoro.jpg','Picoro','guerrero Namekusei','Makancosapo'),
+('./img/roshi.jpg','Maestro Roshi','Maestro de Son Goku','Mafuba');
